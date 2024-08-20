bfc761d7e8595f3e3cc0d9454e7aabf95cad2d66
Chris Skeens <cskee004@odu.edu>
2024-08-20 13:16:16 -0400

    Tested and implemented the toDo class. This class represents a single task that can either be apart of a project or a personal checklist. The ToDo object contains a list of notes that are for that specific task. Notes can be added or removed from the todo task.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 import './modern-normalize.css';
+import './styles.css'
+import { formatDistance, subDays} from "date-fns";
+import {ToDo} from './toDo.js'
+
+const obj = new ToDo("title", "description", "priority" ,"status", "notes", "due date");
+
+console.log(obj);
+
+const notesArray = obj.getNotes();
+
+obj.removeNote(0);
+
+console.log(notesArray);
